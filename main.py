e11a9eba5112de14c4df98727170635c52097225
Kaleb-Aguirre03 <820930@mystma.org>
2021-09-22 09:17:27 -0500

    #1

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-#WRITE YOUR CODE HERE
+#Define Function wordSmash
+def wordSmash (a, b):
+   #return two variables as one combined
+    return a + b
+
+#print the function to create CatDog
+print (wordSmash ("Cat", "Dog"))
+
+#print the function to create Redblue
+print (wordSmash ("Red", "blue"))
+
+#print the function to create qqqqqwwwww
+print (wordSmash ("qqqqq", "wwwww"))
